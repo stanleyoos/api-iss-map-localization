0181556d184177918010acc24a6c3c8dd67bb64e
stanleyoos <stanislaw.sarka@gmail.com>
2020-12-14 15:35:08 +0100

    Setting up the map layer

--- example.js
+++ example.js
@@ -1,3 +1,11 @@
+const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+
 const url_api = "https://api.wheretheiss.at/v1/satellites/25544";
 
 async function getAPI() {
